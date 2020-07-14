87fd5512fd36f5cc5d2883dd7cf46f3bcb875d85
Anton Zuev <zxcv7143@gmail.com>
2020-07-14 12:27:24 +0200

    Initial Commit

---
+++ Tareas/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Tareas
+//
+//  Created by Anton Zuev on 14/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tareas/TareasApp.swift
@@ -0,0 +1,17 @@
+//
+//  TareasApp.swift
+//  Tareas
+//
+//  Created by Anton Zuev on 14/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct TareasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
